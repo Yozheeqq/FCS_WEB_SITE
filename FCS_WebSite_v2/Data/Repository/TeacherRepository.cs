ae3a2254162f79bd6603765008722feb82f9e251
Yozheeqq <90212163+Yozheeqq@users.noreply.github.com>
2023-05-08 23:39:04 +0300

    Убраны границы разметки

--- FCS_WebSite_v2/Data/Repository/TeacherRepository.cs
+++ FCS_WebSite_v2/Data/Repository/TeacherRepository.cs
@@ -13,9 +13,9 @@ namespace FCS_WebSite_v2.Data.Repository
             this.dBContent = dBContent;
         }
 
-        public IEnumerable<Teacher> Pupils => dBContent.Teacher.Include(teacher => teacher.Id);
+        public IEnumerable<Teacher> Teachers => dBContent.Teacher.Include(teacher => teacher.Id);
 
-        public Teacher? GetObjPupil(int id) =>
+        public Teacher? GetObjTeacher(int id) =>
             dBContent.Teacher.FirstOrDefault(teacher => teacher.Id == id.ToString());
 
     }
